9fcdb7ca9e072f69ab1d2f03e30373cb775e86e1
yibei <770893579@qq.com>
2019-12-18 20:06:20 -0500

    /src

---
+++ src/main/java/com/neuedu/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.neuedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication   //入口类
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
